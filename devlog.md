0f9ca4c284f367cd3dc3be029afd37d882459cc8
MarsBarLee <46167686+MarsBarLee@users.noreply.github.com>
2023-08-18 18:01:34 -0400

    Add .py and devlog

---
+++ devlog.md
@@ -0,0 +1,7 @@
+a tic-tac-toe game that insults you at every turn. winning does not guarantee exemption.
+
+2023-08-18
+5 min: Plan on paper
+5 min: Look up 2D array data structure at https://www.scaler.com/topics/2d-array-in-python/
+5 min: Add comments and pseudocode
+5 min: Add data structure 
---
+++ tic-tactless-toe.py
@@ -0,0 +1,15 @@
+# Overarching program
+def tic-tac-toe() {
+    # Data structure: 2D array
+    data_structure = [[], [], []]
+    # Input function
+    # Ask player to make a move
+    # Show state of game in terminal
+    # Check if valid move
+        # if not, reject and ask player to try again
+    # Change state of game by taking player input and updating data strcuture
+    # Update game in terminal
+    # Win logic
+    # Draw logic
+    # Quit logic
+}
